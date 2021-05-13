31625edc5721afaab842762191fc8e147f1a2c04
sivaram <sivaramk1411@gmail.com>
2021-05-13 11:22:16 +0530

    merge the string

--- sample.py
+++ sample.py
@@ -1,12 +1,16 @@
-s=input("Enter some String")
-l=len(s)
-i=0
-while i<l:
-    print(s[i],end=',')
-    i=i+2
-i=1
-while i<l:
-    print(s[i])
-    i=i+2
+s1=input("Enter some String")
+s2=input("Enter Second String")
+i,j=0,0
+output=''
+while i<len(s1)or j<len(s2):
+    if i<len(s1):
+        output=output+s1[i]
+        i=i+1
+    if j<len(s2):
+        output=output+s2[j]
+        j=j+1
+print(output)        
+        
+
     
 
