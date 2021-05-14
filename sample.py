1144b2e41fc0fddd6fd2b8722c12a1af184ddf2e
sivaram <sivaramk1411@gmail.com>
2021-05-14 17:36:55 +0530

    occurence of letters

--- sample.py
+++ sample.py
@@ -1,15 +1,6 @@
-d1=dict({100:"siva",200:"venisha",400:"surendra",300:"nagaraju"})
-print(d1.keys())
-for x in d1.keys():
-    print(x)
-print(d1.values())
-for x in d1.values():
-    print(x)
-print(d1.items())
-for k,v in d1.items():
-    print(k,"---->",v)
-
-
-
-
-
+word=input("enter word")
+d={}
+for x in word:
+    d[x]=d.get(x,0)+1
+for k,v in d.items():
+    print(k,"occured",v,"times")
