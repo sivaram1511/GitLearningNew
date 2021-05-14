818d45bd0d9841260b388930dceeab2db59f8cbc
sivaram <sivaramk1411@gmail.com>
2021-05-14 10:31:26 +0530

    create tuple

--- sample.py
+++ sample.py
@@ -1,16 +1,7 @@
-s=[x for x in range(1,11)]
-print(s)
-v=[x*x  for x in range(1,6)]
-print(v)
-k=[2*x for x in range(1,10)]
-print(k)
-words=["Balaiah","nag","vnkatesh","chiranjeevi"]
-l=[w[0]for w in words]
-print(l)
-s="the quick brown fox jumps over the lazy dog".split()
-print(s)
-l=[[w.upper(),len(w)] for w in s ]
-print(l)        
+t=10,20,30,40
+print(t)
+print(type(t))
+       
 
     
 
