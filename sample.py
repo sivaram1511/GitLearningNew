a117bc068baab5390ec49bb50b152c9ce7194999
sivaram <sivaramk1411@gmail.com>
2021-05-14 10:21:59 +0530

    list comparhension

--- sample.py
+++ sample.py
@@ -1,13 +1,16 @@
-s=input("Enter some String")
-
-l=[]
-for x in s:
-    if x not in l:
-        l.append(x)
-output=''.join(l)
-print(output)
-    
-        
+s=[x for x in range(1,11)]
+print(s)
+v=[x*x  for x in range(1,6)]
+print(v)
+k=[2*x for x in range(1,10)]
+print(k)
+words=["Balaiah","nag","vnkatesh","chiranjeevi"]
+l=[w[0]for w in words]
+print(l)
+s="the quick brown fox jumps over the lazy dog".split()
+print(s)
+l=[[w.upper(),len(w)] for w in s ]
+print(l)        
 
     
 
