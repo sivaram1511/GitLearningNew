dc4ed73350a47ac11c62f6e34752e3107e8736f7
sivaram <sivaramk1411@gmail.com>
2021-05-14 11:08:51 +0530

    add avg variable

--- sample.py
+++ sample.py
@@ -1,8 +1,11 @@
 t=eval(input("Enter Tuple of Numbers:"))
 l=len(t)
 sum=0
+avg=0
 for x in t:
     sum=sum+x
 print("The sum of tuple is:" ,sum)
-print("The sum of avg is :",sum/l)
+avg=sum/l
+print("The avg of tuple is:",avg)
+
 
