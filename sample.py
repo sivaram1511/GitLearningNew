99856bb02779d539d1874e6aed382b92a85e30c7
sivaram <sivaramk1411@gmail.com>
2021-05-14 11:43:42 +0530

    mathmatical operationsof set

--- sample.py
+++ sample.py
@@ -1,11 +1,10 @@
-t=eval(input("Enter Tuple of Numbers:"))
-l=len(t)
-sum=0
-avg=0
-for x in t:
-    sum=sum+x
-print("The sum of tuple is:" ,sum)
-avg=sum/l
-print("The avg of tuple is:",avg)
+x={10,20,30,40}
+y={30,40,50,60}
+print(x|y)
+print(x.intersection(y))# or (x&y)
+print(x.difference(y))#x-y,y-x elements present in x but not in y
+print(x.symmetric_difference(y))#x^y
+
+
 
 
