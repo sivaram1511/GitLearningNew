e57cfdb441eaa282722003df3baf2279a147900e
sivaram <sivaramk1411@gmail.com>
2021-05-14 10:59:44 +0530

    tuple packing, unpacking

--- sample.py
+++ sample.py
@@ -1,7 +1,14 @@
-tt1=(10,20,30,90,76)
-print(tt1)
-print(id(tt1))
-       
+a="sivaram"
+b=[20,79,90]
+c=30
+d=40
+t=a,b,c,d
+print(t)
+p,q,r,s=t
+print("p=",p,"q=",q,"r=",r,"s=",s)
 
+
+
+       
     
 
