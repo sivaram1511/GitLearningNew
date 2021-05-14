4c6f872690a427b7fddfeb717ee1bd4a2a4021b1
sivaram <sivaramk1411@gmail.com>
2021-05-14 17:50:51 +0530

    student info

--- sample.py
+++ sample.py
@@ -1,6 +1,17 @@
-word=input("enter word")
+n=int(input("Enter the number of students"))
 d={}
-for x in word:
-    d[x]=d.get(x,0)+1
-for k,v in d.items():
-    print(k,"occured",v,"times")
+for i in range(n):
+    name=input("name of the student")
+    marks=int(input("Enter marks"))
+    d[name]=marks
+while True:
+    name=input("Enter Student name to get the marks")
+    marks=d.get(name,-1)
+    if marks==-1:
+        print("Stdent not found")
+    else:
+        print("The marks of", name,"are",marks)
+    option=input("do u want find another student marks[yes|no]")
+    if option =="no":
+        break
+print("Thanks for using our applications")    
