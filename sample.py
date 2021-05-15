847f0be0b72a9e0cd8eb0d92e8382e3d49dfadce
sivaram <sivaramk1411@gmail.com>
2021-05-15 12:00:31 +0530

    lambda functions

--- sample.py
+++ sample.py
@@ -1,17 +1,5 @@
-n=int(input("Enter the number of students"))
-d={}
-for i in range(n):
-    name=input("name of the student")
-    marks=int(input("Enter marks"))
-    d[name]=marks
-while True:
-    name=input("Enter Student name to get the marks")
-    marks=d.get(name,-1)
-    if marks==-1:
-        print("Stdent not found")
-    else:
-        print("The marks of", name,"are",marks)
-    option=input("do u want find another student marks[yes|no]")
-    if option =="no":
-        break
-print("Thanks for using our applications")    
+s=lambda n:n*n
+print(s(4))
+
+
+
