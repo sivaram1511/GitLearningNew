a4a05d83efccb1a6f6870cc78a3d51e185fb5c42
sivaram <sivaramk1411@gmail.com>
2021-05-17 16:17:46 +0530

    map,reduce functions

--- sample.py
+++ sample.py
@@ -1,13 +1,15 @@
-def ismul(n):
-    x=0
-    if n%2==0:
-        x=n*2
-    print(x)
-     
-l=[83,45,14,65,18,46,70]
-l1=list(filter(ismul,l))
+from functools import *
+l=[46,42,87,96,58,25,14]
+def doublelt(x):
+    return x*2
+l1=list(map(doublelt,l))
 print(l1)
-print()
+l2=list(map(lambda x:x*2,l))
+print(l2)
+l3=reduce(lambda x,y:x+y,l)
+print(l3)
+l4=reduce(lambda x,y:x*y,l)
+print(l4)
 
         
 
