5bd5deb3c2badea8992e9f0e3e0fedb3a3ca80ba
sivaram <sivaramk1411@gmail.com>
2021-05-17 15:52:59 +0530

    filter function

--- sample.py
+++ sample.py
@@ -1,11 +1,15 @@
-def isEven(n):
+def ismul(n):
+    x=0
     if n%2==0:
-        return True
-    else:
-        return False
-l=[0,5,10,15,20,25,30]
-l1=list(filter(isEven,l))
+        x=n*2
+    print(x)
+     
+l=[83,45,14,65,18,46,70]
+l1=list(filter(ismul,l))
 print(l1)
+print()
+
+        
 
 
 
