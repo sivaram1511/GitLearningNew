5e674bd4a41c439fd35560ab8fb6b2bba2f8ed05
sivaram <sivaramk1411@gmail.com>
2021-05-17 17:17:27 +0530

    name variable uses n module

---
+++ module1.py
@@ -0,0 +1,8 @@
+def f1():
+    if __name__=='__main__':
+        print("The code excuted as a program")
+    else:
+        print("The code excuted as a module form some other program")
+
+f1()        
+
--- sample.py
+++ sample.py
@@ -1,15 +1,7 @@
-from functools import *
-l=[46,42,87,96,58,25,14]
-def doublelt(x):
-    return x*2
-l1=list(map(doublelt,l))
-print(l1)
-l2=list(map(lambda x:x*2,l))
-print(l2)
-l3=reduce(lambda x,y:x+y,l)
-print(l3)
-l4=reduce(lambda x,y:x*y,l)
-print(l4)
+from module1 import f1 as f
+f()
+
+
 
         
 
