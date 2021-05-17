aece13e63208ac31be3705e970c44e5dd7055e8b
sivaram <sivaramk1411@gmail.com>
2021-05-17 17:33:19 +0530

    random()function

--- sample.py
+++ sample.py
@@ -1,7 +1,12 @@
-from module1 import f1 as f
-f()
 
-
+from random import *
+list=["Sunny",76,"Bunny",89,"Chinny",90,"Vinny","Kunny"]
+for i in range(10):
+    #print(random())
+    #print(randint(1,100))
+    #print(uniform(1,10))
+    #print(randrange(10,100,2))
+    print(choice(list))
 
         
 
