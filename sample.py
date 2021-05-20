b6b9f5eccdf560dc74a015c4346b564b14fd60d6
sivaram <sivaramk1411@gmail.com>
2021-05-20 17:22:44 +0530

    has a relation

--- sample.py
+++ sample.py
@@ -1,27 +1,23 @@
-class Car:
-    def __init__(self,name,model,color):
-        self.name=name
-        self.model=model
-        self.color=color
-    def getinfo(self):
-        print("Car name:{},Model:{} and color:{}".format(self.name,self.model,self.color))
+class X:
+    a=10
+    def __init__(self):
+        self.b=20
+    def m1(self):
+        print("m1 method of x class")
+class Y:
+    c=30
+    def __init__(self):
+        self.d=40
+    def m2(self):
+        print("m2 method of Y class")
+    def m3(self):
+        x=X()
+        x.m1()
+        print(x.a,x.b)
+        print(self.c,self.d)
+        self.m2()
+y=Y()
+y.m3()
         
-        
-
-class Employee:
-    def __init__(self,ename,eno,car):
-        self.ename=ename
-        self.eno=eno
-        self.car=car
-    def empinfo(self):
-        print("Name of the Employee:",self.ename)
-        print("Employee Number:",self.eno)
-        print("Employee car info ")
-        self.car.getinfo()
-        
-    
-c=Car("Innova","2.5v","yellow")
-e=Employee("sivaram",101,c)
-e.empinfo()
 
 
