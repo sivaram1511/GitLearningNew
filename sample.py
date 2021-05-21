643f3a93b4550f9318443ecbc396e9acdcd78c3b
sivaram <sivaramk1411@gmail.com>
2021-05-21 15:58:27 +0530

    composition vs aggrigation

--- sample.py
+++ sample.py
@@ -1,26 +1,10 @@
-class X:
-    a=10
-    def __init__(self):
-        self.b=20
-    def m1(self):
-        print("m1 method of x class")
-    @classmethod
-    def m2(cls):
-        print("m2class method  of paren class")
-    @staticmethod
-    def m3():
-        print("parent class static method")
-class Y(X):
-    def __init__(self):
-        self.c=30
-        super().__init__()
-y=Y()
-y.m3()
-y.m2()
-y.m1()
-print(y.a)
-print(y.b)
-print(y.c)
+class Student:
+    collegeName="TJPS"
+    def __init__(self,name):
+        self.name=name
+print(Student.collegeName)
+s=Student("siva")
+print(s.name)
         
 
 
