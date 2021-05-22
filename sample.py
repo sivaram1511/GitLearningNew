6a2d7e94621b4a0ae301b63a970274ebd4f34c1d
sivaram <sivaramk1411@gmail.com>
2021-05-22 16:26:55 +0530

    operator overloadng

--- sample.py
+++ sample.py
@@ -1,23 +1,17 @@
-class P:
-    a=20
-    def __init__(self):
-        print("parent class constuctor")
-        self.b=30
-    def m1(self):
-        print("Parent instance method")
-    @classmethod
-    def m2(cls):
-        print("cls method")
-    @staticmethod
-    def m3():
-        print("static method")
-class C(P):
-    
-    @staticmethod
-    def m2():
-        
-        super(C,C).m3()
-C.m2()
+class Book:
+
+   def __init__(self,pages):
+        self.pages=pages
+   def __add__(self,other):
+        total=self.pages+other.pages
+        return Book(total)
+   def __str__(self):
+       return str(self.pages)
+b1=Book(100)
+b2=Book(200)
+b3=Book(200)
+bx=b1+b2+b3
+print("The total Number of pages",bx)
     
 
         
