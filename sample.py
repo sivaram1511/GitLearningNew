a2130067b42fe082a38b8bc54713df4b57ab011d
sivaram <sivaramk1411@gmail.com>
2021-05-22 17:01:36 +0530

    constructor overloading

--- sample.py
+++ sample.py
@@ -1,13 +1,15 @@
 class P:
-    def property(self):
-        print("Gold+Land+Cash+Power")
+    def __init__(self):
+        print("parent class Constructor")
+    
     def marry(self):
         print("Applamma")
 class C(P):
+    def __init__(self):
+        print("child class Constructor")
     def marry(self):
         super().marry()
         print("Katrina Kaif")
 c=C()
-c.property()
 c.marry()
 
