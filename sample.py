289e4af482877d6ad6985bb1e2787eb624220de0
sivaram <sivaramk1411@gmail.com>
2021-05-22 16:38:22 +0530

    variable number of arguments

--- sample.py
+++ sample.py
@@ -1,11 +1,10 @@
 class Test:
-    def sum(self,a=None,b=None,c=None):
-        if a!=None and b!=None and c!=None:
-            print("The Sum of Three Numbers:",a+b+c)
-        elif a!=None and b!=None:
-            print("The Sum of 2number s",a+b)
-        else:
-            print("please provide 2 or 3 arguments")
+    def sum(self,*a):
+        total=0
+        for x in a:
+            total=total+x
+        print("The Sum:",total)    
+        
 t=Test()
 t.sum(10,20)
 t.sum(10,20,30)
