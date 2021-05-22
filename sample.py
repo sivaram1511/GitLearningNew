5750154514538a5ed5a3466563966480891ce8d6
sivaram <sivaramk1411@gmail.com>
2021-05-22 16:58:12 +0530

    method overriding using super()

--- sample.py
+++ sample.py
@@ -1,18 +1,13 @@
-class Test:
-    def __init__(self,a=None,b=None,c=None):
-        if a!=None and b!=None and c!=None:
-            print("Sum os a b c",a+b+c)
-        elif a!=None and b!=None:
-            print("Sum of a,b",a+b)
-        else:
-            print("at least two argumnets")
-
-       
-        
-
-t1=Test()
-t2=Test(10,20)
-t3=Test(10,20,30)
-        
-
+class P:
+    def property(self):
+        print("Gold+Land+Cash+Power")
+    def marry(self):
+        print("Applamma")
+class C(P):
+    def marry(self):
+        super().marry()
+        print("Katrina Kaif")
+c=C()
+c.property()
+c.marry()
 
