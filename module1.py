a53366d91f184adb1b68a84698c0403d101418e3
sivaram <sivaramk1411@gmail.com>
2021-05-28 12:01:25 +0530

    create csv file with employee details

--- module1.py
+++ module1.py
@@ -1,5 +1,17 @@
-f=open("abc.txt",'w')
-list=["sunny\n","bunny\n","vinny\n","chunny\n"]
-f.writelines(list)
-print("List of lines written to the file success")
-f.close()
+import csv
+with open("emp.csv","w",newline='')as f:
+    w=csv.writer(f)
+    w.writerow(["Eno","Ename","Esal","Eaddr"])
+    n=int(input("Enter number ofemployee:"))
+    for i in range(n):
+        eno=input("enter employee No:")
+        ename=input("Enter Employee Name:")
+        esal=input("enter employee salary:")
+        eaddr=input("enter employee address:")
+        w.writerow([eno,ename,esal,eaddr])
+print("csv file successfuly")        
+
+
+
+
+
